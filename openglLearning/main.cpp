07d56bcdbec684a2a190705f0a70ba5a226743ef
yigudove <yigudove@outlook.com>
2026-08-18 10:42:49 +0800

    窗口

---
+++ openglLearning/main.cpp
@@ -0,0 +1,64 @@
+//
+// Created by yigudove on 2026/8/18.
+//
+
+#include "main.h"
+
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+using namespace std;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+int main()
+{
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+#endif
+    cout << "程序入口" << endl;
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+    if (window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // 定义加载系统相关的OpenGL函数指针地址的函数
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+
+    // 注册调整窗口大小的函数
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    return 0;
+}
+
---
+++ openglLearning/main.h
@@ -0,0 +1,14 @@
+//
+// Created by yigudove on 2026/8/18.
+//
+
+#ifndef OPENGLLEARNING_MAIN_H
+#define OPENGLLEARNING_MAIN_H
+
+
+class main
+{
+};
+
+
+#endif //OPENGLLEARNING_MAIN_H
